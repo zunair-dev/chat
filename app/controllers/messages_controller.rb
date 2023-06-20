4850f0658318cc53416454cc1e15ec5965b3ad27
Zunair <zunair.dev@gmail.com>
2023-06-20 16:17:36 +0500

    update the messages controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -20,6 +20,6 @@ class MessagesController < ApplicationController
   end
 
   def message_params
-    params.require(:message).permit(:content)
+    params.require(:message).permit(:context)
   end
 end
